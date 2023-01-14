547f04b8eaddc335f0e0339d996362bd435d7fb4
Krzysztof Perwel <kperwel@evolution.com>
2023-01-14 20:21:34 +0100

    feat: some example favicons

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,25 @@ export default defineConfig({
         clientsClaim: true,
         skipWaiting: true,
       },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'pwa.svg'],
+      manifest: {
+        name: "The Jam Game",
+        short_name: "The Jam",
+        description: "The Game bade by @kperwel for a gamejam",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
     }),
   ],
 });
