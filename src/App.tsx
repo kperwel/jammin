bfa188b39ff5fb4c7d975109d8e2a2eb751bdc09
Krzysztof Perwel <kperwel@evolution.com>
2023-01-14 10:05:10 +0100

    feat: add initial empty scene

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,45 @@
-import "./App.css";
+import { useRef, useState } from "react";
+import { Canvas, MeshProps, useFrame } from "@react-three/fiber";
+import { Mesh } from "three";
+
+function Box(props: MeshProps) {
+  // This reference will give us direct access to the mesh
+  const mesh = useRef<Mesh | null>(null);
+  // Set up state for the hovered and active state
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  useFrame((_state, delta) => {
+    if (mesh.current === null) {
+      return;
+    }
+    return (mesh.current.rotation.x += delta);
+  });
+  // Return view, these are regular three.js elements expressed in JSX
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? 1.5 : 1}
+      onClick={(event) => setActive(!active)}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+    </mesh>
+  );
+}
 
 function App() {
-  return null;
+  return (
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+    </Canvas>
+  );
 }
 
 export default App;
